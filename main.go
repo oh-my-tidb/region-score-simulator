65cf3eebbe0f5abe1260b661d92c2b7a0ccd43c2
tongjian <1045931706@qq.com>
2021-04-01 20:35:57 +0800

    add legend add x label and value add zoom to  zoom down/up area add tooltip to view point data
    Signed-off-by: tongjian <1045931706@qq.com>

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ var port = flag.String("p", ":8081", "serving addr")
 
 func main() {
 	flag.Parse()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		stores := defaultStores
@@ -57,6 +58,8 @@ func main() {
 		charts := genChart(stores, amps, deadSpaces, k, m, f)
 		myRender(w, charts)
 	})
+
+	fmt.Printf("server start,listen: http://localhost%s", *port)
 	http.ListenAndServe(*port, nil)
 }
 
@@ -101,7 +104,7 @@ func score(R, C, A, K, M, F float64) float64 {
 
 func genChart(Cs, Amps, Ds []float64, K, M, F float64) []render.Renderer {
 	Rs := make([]float64, len(Cs))
-	var xAxis []string
+	var xAxis []float64
 	sizeData := make([][]opts.LineData, len(Cs))
 	availableData := make([][]opts.LineData, len(Cs))
 	percentData := make([][]opts.LineData, len(Cs))
@@ -128,7 +131,7 @@ func genChart(Cs, Amps, Ds []float64, K, M, F float64) []render.Renderer {
 
 		Rs[minIndex[rand.Intn(len(minIndex))]] += 0.1
 		if i%50 == 0 {
-			xAxis = append(xAxis, "")
+			xAxis = append(xAxis, Rs[0])
 			for j := range Rs {
 				sizeData[j] = append(sizeData[j], opts.LineData{Value: Rs[j]})
 				A := Cs[j] - Ds[j] - Rs[j]*Amps[j]
@@ -141,49 +144,70 @@ func genChart(Cs, Amps, Ds []float64, K, M, F float64) []render.Renderer {
 		}
 	}
 
+	legends := make([]string, len(Cs))
+	for i := range Cs {
+		legends[i] = fmt.Sprintf("s-%d", i)
+	}
 	size := charts.NewLine()
-	size.SetGlobalOptions(
-		charts.WithTitleOpts(opts.Title{
-			Title: "Size",
-		}),
-	)
+	size.SetGlobalOptions(setDefaultOption(legends)...)
+
 	size.SetXAxis(xAxis)
 	for i := range sizeData {
-		size.AddSeries(fmt.Sprintf("s%.0f", Cs[i]), sizeData[i])
+		size.AddSeries(fmt.Sprintf("s-%d", i), sizeData[i])
 	}
 
 	available := charts.NewLine()
-	available.SetGlobalOptions(
-		charts.WithTitleOpts(opts.Title{
-			Title: "Available",
-		}),
-	)
+	available.SetGlobalOptions(setDefaultOption(legends)...)
 	available.SetXAxis(xAxis)
 	for i := range availableData {
-		available.AddSeries(fmt.Sprintf("s%.0f", Cs[i]), availableData[i])
+		available.AddSeries(fmt.Sprintf("s-%d", i), availableData[i])
 	}
 
 	percent := charts.NewLine()
-	percent.SetGlobalOptions(
-		charts.WithTitleOpts(opts.Title{
-			Title: "Percent",
-		}),
-	)
+	percent.SetGlobalOptions(setDefaultOption(legends)...)
 	percent.SetXAxis(xAxis)
 	for i := range percentData {
-		percent.AddSeries(fmt.Sprintf("s%.0f", Cs[i]), percentData[i])
+		percent.AddSeries(fmt.Sprintf("s-%d", i), percentData[i])
 	}
 
 	score := charts.NewLine()
-	score.SetGlobalOptions(
-		charts.WithTitleOpts(opts.Title{
-			Title: "Score",
-		}),
-	)
+	score.SetGlobalOptions(setDefaultOption(legends)...)
 	score.SetXAxis(xAxis)
 	for i := range scoreData {
-		score.AddSeries(fmt.Sprintf("s%.0f", Cs[i]), scoreData[i])
+		score.AddSeries(fmt.Sprintf("s-%d", i), scoreData[i])
 	}
 
 	return []render.Renderer{size, available, percent, score}
+}
+
+func setDefaultOption(legends []string) []charts.GlobalOpts {
+	return []charts.GlobalOpts{
+		charts.WithTitleOpts(opts.Title{
+			Title: "Score",
+		}),
+		charts.WithTooltipOpts(opts.Tooltip{
+			Show:    true,
+			Trigger: "axis",
+		}),
+		charts.WithLegendOpts(opts.Legend{
+			Data: legends,
+			Show: true,
+		}),
+		charts.WithXAxisOpts(opts.XAxis{
+			Show: true,
+			Name: "used size",
+		}, 0),
+		charts.WithToolboxOpts(opts.Toolbox{
+			Show:  true,
+			Right: "20%",
+			Feature: &opts.ToolBoxFeature{
+				SaveAsImage: &opts.ToolBoxFeatureSaveAsImage{
+					Show: true,
+				},
+				DataZoom: &opts.ToolBoxFeatureDataZoom{
+					Show: true,
+				},
+			},
+		}),
+	}
 }
